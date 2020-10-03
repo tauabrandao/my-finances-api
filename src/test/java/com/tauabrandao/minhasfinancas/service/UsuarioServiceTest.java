0be2309ee6922d5a39942454c10bf8c003a868df
Tauã Brandão <brandao.taua@gmail.com>
2020-10-03 14:24:09 -0300

    Test to validate if you are throwing an exception when there is already a registered user with the informed email

--- src/test/java/com/tauabrandao/minhasfinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/tauabrandao/minhasfinancas/service/UsuarioServiceTest.java
@@ -1,5 +1,6 @@
 package com.tauabrandao.minhasfinancas.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.tauabrandao.minhasfinancas.exception.RegraNegocioException;
+import com.tauabrandao.minhasfinancas.model.entity.Usuario;
 import com.tauabrandao.minhasfinancas.model.repository.UsuarioRepository;
 
 @SpringBootTest
@@ -22,11 +25,20 @@ public class UsuarioServiceTest {
 
 	@Test
 	public void deveValidarEmail() {
+		Assertions.assertDoesNotThrow(() -> {
+			repository.deleteAll();
+			service.validarEmail("usuario@email.com");
+		});
+	}
 
-		repository.deleteAll();
-
-		service.validarEmail("usuario@email.com");
-
+	@Test
+	public void deveLancarExcessaoAoValidarEmailQuandoExistirUsuarioComOEmailInformado() {
+		String email = "email@email.com";
+		Assertions.assertThrows(RegraNegocioException.class, () -> {
+			Usuario usuario = Usuario.builder().nome("nome do usuario").email(email).build();
+			repository.save(usuario);
+			service.validarEmail(email);
+		});
 	}
 
 }
