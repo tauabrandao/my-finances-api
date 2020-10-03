9c9e49280180d5f01fa7ac18725b2ffa1b8822af
Tauã Brandão <brandao.taua@gmail.com>
2020-10-03 10:27:40 -0300

    Configuring the connection to the database Mapping Usuario entity

---
+++ src/main/java/com/tauabrandao/minhasfinancas/model/entity/Usuario.java
@@ -0,0 +1,41 @@
+package com.tauabrandao.minhasfinancas.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "usuario", schema = "financas")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Usuario {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "nome")
+	private String nome;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "senha")
+	private String senha;
+
+}
