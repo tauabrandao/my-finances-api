1ecbdb3b559461b2305e890595bc18a5286e7c1c
Tauã Brandão <brandao.taua@gmail.com>
2020-10-03 13:34:11 -0300

    test profile and in-memory database configuration for testing

--- src/test/java/com/tauabrandao/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/tauabrandao/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.tauabrandao.minhasfinancas.model.entity.Usuario;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 public class UsuarioRepositoryTest {
 
 	@Autowired
@@ -18,19 +20,30 @@ public class UsuarioRepositoryTest {
 
 	@Test
 	public void deveVerificarAExistenciaDeUmEmail() {
-
 		String email = "usuario@email.com";
-
 		// cenario
 		Usuario usuario = Usuario.builder().nome("nome do usuario").email(email).build();
 		repository.save(usuario);
+		
 		// Ação / Execução
-		boolean result = repository.existsByEmail(email);
-
+		boolean usuarioExiste = repository.existsByEmail(email);
+		
 		// verificação
-
-		Assertions.assertThat(result).isTrue();
-
+		Assertions.assertThat(usuarioExiste).isTrue();
+	}
+	
+	@Test
+	public void deveRetornarFalsoQuandoNaoHouverUsuarioCadastradoComEmailInformado() {
+		
+		String email = "usuario@usuario.com.br";
+		
+		Usuario usuario = Usuario.builder().nome("nome do usuario").email(email).build();
+		repository.save(usuario);
+		
+		boolean usuarioExiste = repository.existsByEmail("qualquer@usuario.com");
+		
+		Assertions.assertThat(usuarioExiste).isFalse();
+		
 	}
 
 }
