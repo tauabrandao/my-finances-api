ae439741e7e40ff02177a9001fc47040eac41d30
Tauã Brandão <brandao.taua@gmail.com>
2020-10-24 19:48:20 -0300

    Automatic creation of tables in the bank, Creation of endpoints to save and delete Lancamento

--- src/main/java/com/tauabrandao/minhasfinancas/api/controller/LancamentoController.java
+++ src/main/java/com/tauabrandao/minhasfinancas/api/controller/LancamentoController.java
@@ -1,13 +1,19 @@
 package com.tauabrandao.minhasfinancas.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tauabrandao.minhasfinancas.api.dto.LancamentoDTO;
@@ -33,6 +39,7 @@ public class LancamentoController {
 	public ResponseEntity<Object> salvar(@RequestBody LancamentoDTO dto) {
 		try {
 			Lancamento entidade = converter(dto);
+			entidade.setStatus(StatusLancamento.PENDENTE);
 			entidade = service.salvar(entidade);
 			return new ResponseEntity<Object>(entidade, HttpStatus.CREATED);
 		} catch (RegraNegocioException e) {
@@ -53,6 +60,39 @@ public class LancamentoController {
 			}
 		}).orElseGet(() -> new ResponseEntity("Lançamento não encontrado na base de dados.", HttpStatus.BAD_REQUEST));
 	}
+	
+	@GetMapping
+	public ResponseEntity buscar(
+			@RequestParam(value = "descricao", required = false) String descricao,
+			@RequestParam(value = "mes", required = false) Integer mes,
+			@RequestParam(value = "ano", required = false) Integer ano,
+			@RequestParam(value = "usuario") Long idUsuario
+			) {
+		
+		Lancamento lancamentoFiltro = Lancamento.builder()
+				.descricao(descricao).mes(mes).ano(ano).build();
+		
+		Optional<Usuario> usuario = usuarioService.getById(idUsuario);
+		if(!usuario.isPresent()) {
+			return ResponseEntity.badRequest().body("Não foi possível realizar a consulta. Usuário não encontrado para o id informado.");
+		}else {
+			lancamentoFiltro.setUsuario(usuario.get());
+		}
+		
+		List<Lancamento> lancamentos = service.buscar(lancamentoFiltro);
+		return ResponseEntity.ok(lancamentos);
+		
+	}
+	
+	@DeleteMapping("{id}")
+	private ResponseEntity deletar(@PathVariable("id") Long id) {
+		return service.getById(id).map(entity -> {
+			service.deletar(entity);
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}).orElseGet(() -> 
+			new ResponseEntity("Lançamento não encontrado na base de Dados.", HttpStatus.BAD_REQUEST)
+		);
+	}
 
 	private Lancamento converter(LancamentoDTO dto) {
 		Lancamento lancamento = new Lancamento();
@@ -60,11 +100,17 @@ public class LancamentoController {
 		lancamento.setDescricao(dto.getDescricao());
 		lancamento.setId(dto.getId());
 		lancamento.setMes(dto.getMes());
-		lancamento.setTipo(TipoLancamento.valueOf(dto.getTipo()));
-		lancamento.setStatus(StatusLancamento.valueOf(dto.getStatus()));
+		
+		if(dto.getTipo() != null) {
+			lancamento.setTipo(TipoLancamento.valueOf(dto.getTipo()));			
+		}
+		if(dto.getStatus() !=null) {
+			lancamento.setStatus(StatusLancamento.valueOf(dto.getStatus()));			
+		}
+		
 		lancamento.setValor(dto.getValor());
 
-		Usuario usuario = usuarioService.getById(dto.getId())
+		Usuario usuario = usuarioService.getById(dto.getUsuario())
 				.orElseThrow(() -> new RegraNegocioException("Usuário não encontrado para o ID informado"));
 		lancamento.setUsuario(usuario);
 
