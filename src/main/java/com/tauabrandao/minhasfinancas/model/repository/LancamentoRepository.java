6d357e74416e26b9d6c77c259201bd1b8cfa7890
Tauã Brandão <tauabrandao@MacBook-Pro-de-Taua.local>
2020-12-24 18:47:13 -0300

    Adjustments to show balance

--- src/main/java/com/tauabrandao/minhasfinancas/model/repository/LancamentoRepository.java
+++ src/main/java/com/tauabrandao/minhasfinancas/model/repository/LancamentoRepository.java
@@ -7,12 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.tauabrandao.minhasfinancas.model.entity.Lancamento;
+import com.tauabrandao.minhasfinancas.model.enums.StatusLancamento;
 import com.tauabrandao.minhasfinancas.model.enums.TipoLancamento;
 
-public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 
 	@Query(value = " select sum(l.valor) from Lancamento l join l.usuario u"
-			+ " where u.id = :pIdUsuario and l.tipo = :pTipo group by u")
-	BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("pIdUsuario") Long idUsuario, @Param("pTipo") TipoLancamento tipo);
-	
+			+ " where u.id = :pIdUsuario and l.tipo = :pTipo and l.status = :status group by u")
+	BigDecimal obterSaldoPorTipoLancamentoEUsuarioEStatus(@Param("pIdUsuario") Long idUsuario,
+			@Param("pTipo") TipoLancamento tipo, @Param("status") StatusLancamento status);
+
 }
