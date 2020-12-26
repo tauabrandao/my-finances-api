157d6c43b06da4a21ff998d1b68becd2424165ee
Tauã Brandão <tauabrandao@MacBook-Pro-de-Taua.local>
2020-12-26 02:37:07 -0300

    Web configuration adjustments

--- src/main/java/com/tauabrandao/minhasfinancas/MinhasfinancasApplication.java
+++ src/main/java/com/tauabrandao/minhasfinancas/MinhasfinancasApplication.java
@@ -7,16 +7,10 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableWebMvc
 public class MinhasfinancasApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MinhasfinancasApplication.class, args);
 	}
 	
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-	}
-
 }
